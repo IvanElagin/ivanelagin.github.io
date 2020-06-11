9eb17bad0acd3517847a4f1637ea4d92a079d27d
IvanElagin <65159184+IvanElagin@users.noreply.github.com>
2020-06-11 22:31:20 +0300

    Add files via upload

---
+++ mainP.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector("#menu_open");
+
+menuToggle.onclick = function (){
+  menuopencontaine.classList.toggle("menu_open--active");
+}
+/*document.querySelector(".menu_icon_wrapper").onclick = function () {
+  document.querySelector(".menu_icon").classList.toggle("menu_icon_active");
+}*/
